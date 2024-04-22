5ef16b797aa503cedc0eb2f819ff60e6e04ecc82
pakjonghun <150112208+pakpakjonghun@users.noreply.github.com>
2024-04-22 15:18:38 +0900

    테스트 api

--- src/app/one/page.tsx
+++ src/app/one/page.tsx
@@ -1,10 +1,22 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import { useRouter } from 'next/navigation';
 
 const OnePage = () => {
+  const url = 'https://api.thecatapi.com/v1/images/search?limit=10';
+  const getUrl = () => axios(url).then((res) => res.data);
+  const { data } = useQuery({ queryFn: getUrl, queryKey: ['url'] });
+  console.log(data);
   const router = useRouter();
-  return <button onClick={() => router.replace('/two')}>go two</button>;
+  return (
+    <>
+      {data?.map((item: any) => JSON.stringify(item))}
+      <br />
+      <button onClick={() => router.replace('/two')}>go two</button>
+    </>
+  );
 };
 
 export default OnePage;
